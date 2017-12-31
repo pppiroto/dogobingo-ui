6daa16ceabd568fb0043f60c5b899b43b08b805a
Hiroto Yagi <pppiroto@gmail.com>
2017-12-31 21:54:56 +0900

    初期環境

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { AlertModule } from 'ngx-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, AlertModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
