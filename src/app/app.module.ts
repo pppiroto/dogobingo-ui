5dcf9f495197fcee0cb7705e6d5d42a35cd9e314
Hiroto Yagi <pppiroto@gmail.com>
2018-01-01 13:41:32 +0900

    LOGO

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-import { AlertModule } from 'ngx-bootstrap';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, AlertModule.forRoot()
+    BrowserModule, 
+    HttpModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
