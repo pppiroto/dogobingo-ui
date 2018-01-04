b09651e582ac7bde9e55df7b2bbe06b535ba7bf7
Hiroto Yagi <pppiroto@gmail.com>
2018-01-04 16:04:29 +0900

    SNS Login status

--- src/app/navi/navi.component.ts
+++ src/app/navi/navi.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { SnsAccount } from '../sns-account.enum';
 declare const hello;
 
 @Component({
@@ -8,17 +8,56 @@ declare const hello;
   styleUrls: ['./navi.component.css']
 })
 export class NaviComponent implements OnInit {
-  userName: string = "TEST";
+  userName: string;
+
   constructor() { 
   }
   ngOnInit() {
-    // // TODO
-    // hello("facebook").api('me').then(function(json) {
-    //   console.log(`B! ${json.name}`);
-    //   this.userName = json.name;
-    // }, function(e) {
-    //   console.log(`B! ERROR`);
-    //   this.userName = "ERROR";
-    // });  
+    this.getLoginStatus();
+  }
+
+  /**
+   * ユーザーのSNS認証ログイン状態の取得
+   */
+  getLoginStatus() {
+    var self = this;
+    this.userName = null; 
+
+    var promise = this.getSnsUserInfoPromise(SnsAccount.facebook)
+      .then(function(json:any){
+        if (self.userName == null) self.userName = json.name;
+      })
+      .catch(function(e:any) { 
+        return this.getSnsUserInfoPromise(SnsAccount.twitter);
+      })
+      .then(function(json:any){
+        if (self.userName == null) self.userName = json.name;
+      })
+      .catch(function(e:any) { 
+        self.userName = "Please login..."
+      })
+      ;
+  }
+
+  /**
+   * ログインユーザー情報を取得
+   * 
+   * @param sns 
+   */
+  getSnsUserInfoPromise(sns:string) {
+    return new Promise(
+      function(resolve, reject) {
+        hello(sns).api('me').then(
+          function(json) {
+            console.log(`B! ${sns} name:${json.name}`);
+            resolve(json);
+          }, 
+          function(e) {
+            console.log(`B! ${sns} error:${e.error.message}`);
+            reject(e.error.message);
+          }
+        );  
+      }
+    );
   }
 }
